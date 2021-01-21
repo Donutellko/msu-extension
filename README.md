b26ff4cf58a81fc64bba58124af416271cd98440
Шергалис Донат Витальевич <dshergalis@alfabank.ru>
2021-01-22 01:21:38 +0300

    v1.0: spbstu/autologin

---
+++ README.md
@@ -0,0 +1,7 @@
+# Расширение для улучшения UX монлога пайпа
+
+## Changelog 
+
+### v1.0: add `autologin.js`
+Автоклик на кнопке Войти вот [тут](https://cas.spbstu.ru/login);
+
---
+++ spbstu/autologin.js
@@ -0,0 +1,34 @@
+console.log("Started autologin extension")
+
+const LOCAL_STORAGE_PLATFORM_USERNAME = 'spbstu_username'
+const LOCAL_STORAGE_PLATFORM_PASSWORD = 'spbstu_password'
+
+let usernameField = document.getElementById('user')
+let passwordField = document.getElementById('password')
+
+let loginButton = document.getElementById('doLogin')
+//
+let savedUsername = localStorage[LOCAL_STORAGE_PLATFORM_USERNAME]
+let savedPassword = localStorage[LOCAL_STORAGE_PLATFORM_PASSWORD]
+
+if (!usernameField.value && savedUsername) usernameField.value = savedUsername
+if (!passwordField.value && savedPassword) passwordField.value = savedPassword
+
+let errorText = document.querySelector('.form_alert')
+
+if (usernameField.value
+    && passwordField.value
+    && !errorText) {
+
+    console.log("CLICK!", usernameField.value, passwordField.value)
+    loginButton.disabled = false
+    loginButton.click()
+    loginButton.style.background = 'grey'
+    loginButton.style.cursor = 'wait'
+
+} else {
+    loginButton.onclick = () => {
+        localStorage[LOCAL_STORAGE_PLATFORM_USERNAME] = usernameField.value
+        localStorage[LOCAL_STORAGE_PLATFORM_PASSWORD] = passwordField.value
+    }
+}
